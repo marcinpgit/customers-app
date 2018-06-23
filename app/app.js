6c8d6dba9d51c0e1c0caa5d128945d452924715e
marcinpgit <marcinp2012@gmail.com>
2018-06-23 21:51:39 +0200

    adding ng-view to a page

--- app/app.js
+++ app/app.js
@@ -1,14 +1,14 @@
 (function() {
-
+    
     var app = angular.module('customersApp', ['ngRoute']);
-
+    
     app.config(function($routeProvider) {
         $routeProvider
             .when('/', {
                 controller: 'CustomersController',
                 templateUrl: 'app/views/customers.html'
             })
-            .otherwise({ redirectTo: '/' });
+            .otherwise( { redirectTo: '/' } );
     });
-
+    
 }());
