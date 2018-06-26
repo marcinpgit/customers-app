3cf79265f87a37cc478222521bfbb37c3b14ae8b
marcinpgit <marcinp2012@gmail.com>
2018-06-26 13:53:50 +0200

    uding the  service

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,6 +1,6 @@
 (function() {
     
-    var CustomersController = function ($scope, customersFactory, appSettings) {
+    var CustomersController = function ($scope, $log, customersFactory, appSettings) {
         $scope.sortBy = 'name';
         $scope.reverse = false;
         $scope.customers = [];
@@ -12,7 +12,7 @@
                     $scope.customers = customers;
                 })
                 .error(function(data, status, headers, config) {
-                    //handle error
+                    $log.log(data.error + '' + status);
                 });
         };
 
@@ -24,7 +24,7 @@
         };
     };
     
-    CustomersController.$inject = ['$scope', 'customersFactory', 'appSettings'];
+    CustomersController.$inject = ['$scope', '$log', 'customersFactory', 'appSettings'];
 
     angular.module('customersApp')
       .controller('CustomersController', CustomersController);
--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.get('/customers/:id', function(req, res) {
 });
 
 app.get('/customers', function(req, res) {
-    res.json(customers);
-    //res.json(500, { error: 'An error has occurred!' });
+    // res.json(customers);
+    res.json(500, { error: 'An error has occurred!' });
 });
 
 var customers = [
